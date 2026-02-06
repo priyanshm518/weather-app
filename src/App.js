18c3bb8931f8a8b1d61cba48598b5a7755492328
Priyansh <priyanshm518@gmail.com>
2026-02-06 12:25:49 +0530

    fix eslint ci error

--- src/App.js
+++ src/App.js
@@ -233,6 +233,7 @@ function App() {
   };
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
+// eslint-disable-next-line react-hooks/exhaustive-deps
 useEffect(() => {
   fetchWeather("New York");
 }, [unit]);
